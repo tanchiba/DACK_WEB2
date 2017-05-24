420f365c66d9d1b1bd768c3fbb3c5afe194bafd7
DESKTOP-VQM4KIO\ASUS <mychikhtn@gmail.com>
2017-05-24 11:54:54 +0700

    Producer, PageList, Details

---
+++ MobileShop/Controllers/NhaSanXuatController.cs
@@ -0,0 +1,20 @@
+﻿using MobileShop.Models.BUS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+
+namespace MobileShop.Controllers
+{
+    public class NhaSanXuatController : Controller
+    {
+        // GET: NhaSanXuat
+        public ActionResult Index(int id, int page = 1, int pagesize = 3)
+        {
+            var ds = NhaSanXuatBUS.ChiTiet(id).ToPagedList(page, pagesize);
+            return View(ds);
+        }
+    }
+}
---
+++ MobileShop/Models/BUS/NhaSanXuatBUS.cs
@@ -0,0 +1,25 @@
+﻿using MobileShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileShop.Models.BUS
+{
+    public class NhaSanXuatBUS
+    {
+        public static IEnumerable<NhaSanXuat> DanhSach()
+        {
+            var db = new MobileShopConnectionDB();
+            return db.Query<NhaSanXuat>("select * from NhaSanXuat where BiXoa = 0");
+
+        }
+
+        public static IEnumerable<SanPham> ChiTiet(int id)
+        {
+            var db = new MobileShopConnectionDB();
+            return db.Query<SanPham>("select * from SanPham where MaNhaSanXuat = @0", id);
+
+        }
+    }
+ }
