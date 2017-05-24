6475bc2331e2d19fb7a4ce8842ee9ab1dc719f1a
LeVanChiu <lvchiu94@gmail.com>
2017-05-23 23:28:03 -0800

    Create Area Admin

---
+++ MobileShop/Areas/Admin/Models/SanPhamaAdminBus/SanPhamAdminBus.cs
@@ -0,0 +1,17 @@
+﻿using MobileShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileShop.Areas.Admin.Models.SanPhamaAdminBus
+{
+    public class SanPhamAdminBus
+    {
+        public static IEnumerable<SanPham> DanhSach()
+        {
+            var db = new MobileShopConnectionDB();
+            return db.Query<SanPham>("select  * from SanPham");
+        }
+    }
+}
