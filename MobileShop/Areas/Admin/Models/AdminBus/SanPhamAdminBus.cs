dbcfc00d7c04816d4f43c49b578e0676a1d94cd7
LeVanChiu <lvchiu94@gmail.com>
2017-05-23 20:33:33 -0800

    Create SanPhamadminContreller, SanPhamAdminBus

---
+++ MobileShop/Areas/Admin/Models/AdminBus/SanPhamAdminBus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileShop.Areas.Admin.Models.AdminBus
+{
+    public class SanPhamAdminBus
+    {
+        //public static IEnumerable<SanPham> DanhSach()
+        //{
+        //    var db = new ShopMoBileConnectionDB();
+        //    return db.Query<SanPham>("select  * from SanPham");
+        //}
+    }
+}
