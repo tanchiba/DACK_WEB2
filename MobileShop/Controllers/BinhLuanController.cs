b336fd52cc1481763d2acfb7ce6678a329131b7c
DESKTOP-VQM4KIO\ASUS <mychikhtn@gmail.com>
2017-06-10 18:23:54 +0700

    Edit comment Product

---
+++ MobileShop/Controllers/BinhLuanController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity;
+using MobileShop.Models.BUS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MobileShop.Views
+{
+    public class BinhLuanController : Controller
+    {
+        // GET: BinhLuan
+        [Authorize]
+        public ActionResult Create(int MaSanPham = 0, String NoiDung ="")
+        {
+            if (MaSanPham == 0)
+                return Redirect("/");
+            BinhLuanBUS.Them(MaSanPham, User.Identity.GetUserId(), User.Identity.Name, NoiDung);
+            return RedirectToAction("Details", "SanPham", new { Id = MaSanPham});
+        }
+
+        public ActionResult Index(int MaSanPham)
+        {
+            ViewBag.MaSanPham = MaSanPham;
+            return View(BinhLuanBUS.DanhSach(MaSanPham));
+        }
+    }
+}
--- MobileShop/Models/Generated/Database.cs
+++ MobileShop/Models/Generated/Database.cs
@@ -8,7 +8,7 @@
 //     Provider:               `System.Data.SqlClient`
 //     Connection String:      `Data Source=(local);Initial Catalog=MobileShop;Integrated Security=True`
 //     Schema:                 ``
-//     Include Views:          `False`
+//     Include Views:          `True`
 
 using System;
 using System.Collections.Generic;
@@ -307,5 +307,36 @@ namespace MobileShopConnection
     {
 		[Column] public int MaTinhTrang { get; set; }
 		[Column] public string TenTinhTrang { get; set; }
+	}
+    
+	[TableName("dbo.v_ChiTietSanPham")]
+	[ExplicitColumns]
+    public partial class v_ChiTietSanPham : MobileShopConnectionDB.Record<v_ChiTietSanPham>  
+    {
+		[Column] public int MaSanPham { get; set; }
+		[Column] public string TenSanPham { get; set; }
+		[Column] public string MoTa { get; set; }
+		[Column] public string XuatXu { get; set; }
+		[Column] public int MaNhaSanXuat { get; set; }
+		[Column] public int? GiaBan { get; set; }
+		[Column] public DateTime? NgayNhap { get; set; }
+		[Column] public int? SoLuongBan { get; set; }
+		[Column] public int? SoLuongTon { get; set; }
+		[Column] public int MaLoaiSanPham { get; set; }
+		[Column] public int? SoLuongXem { get; set; }
+		[Column] public string HinhAnh { get; set; }
+		[Column] public byte? BiXoa { get; set; }
+		[Column] public string TenNhaSanXuat { get; set; }
+	}
+    
+	[TableName("dbo.v_GioHang")]
+	[ExplicitColumns]
+    public partial class v_GioHang : MobileShopConnectionDB.Record<v_GioHang>  
+    {
+		[Column] public int Id { get; set; }
+		[Column] public string MaTaiKhoan { get; set; }
+		[Column] public int MaSanPham { get; set; }
+		[Column] public int SoLuong { get; set; }
+		[Column] public string TenSanPham { get; set; }
 	}
 }
