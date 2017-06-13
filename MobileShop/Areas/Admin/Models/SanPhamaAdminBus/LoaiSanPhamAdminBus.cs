87dbfbb8f9a2f5877e5e9b46697e79e8d0da2719
LeVanChiu <lvchiu94@gmail.com>
2017-06-12 21:13:06 -0800

    Create LoaiSanPhamBus

---
+++ MobileShop/Areas/Admin/Models/SanPhamaAdminBus/LoaiSanPhamAdminBus.cs
@@ -0,0 +1,17 @@
+﻿using MobileShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileShop.Areas.Admin.Models.SanPhamaAdminBus
+{
+    public class LoaiSanPhamAdminBus
+    {
+        public static IEnumerable<LoaiSanPham> DanhSach()
+        {
+            var dsLoaiSanPham = new MobileShopConnectionDB();
+            return dsLoaiSanPham.Query<LoaiSanPham>("select * from loaisanpham");
+        }
+    }
+}
