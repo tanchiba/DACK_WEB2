bbacdaeade99fa94333dc467f2c967ec7879420f
ToanTran <trctoan@gmail.com>
2017-06-18 15:44:05 +0700

    Action: Create SearchController and Create SearchBUS

---
+++ MobileShop/Models/BUS/SearchBUS.cs
@@ -0,0 +1,19 @@
+﻿using MobileShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileShop.Models.BUS
+{
+    public class SearchBUS
+    {
+        public static IEnumerable<SanPham> LoadDSSanPham(String KeyWord)
+        {
+            using (var db = new MobileShopConnectionDB())
+            {
+                return db.Query<SanPham>("SELECT * FROM SanPham WHERE TenSanPham = @0", KeyWord);
+            }
+        }
+    }
+}
