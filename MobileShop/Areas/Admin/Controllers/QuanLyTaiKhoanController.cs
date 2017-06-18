b2236929e4f63c0a4f6c3a81b866fd816e26e95e
LeVanChiu <lvchiu94@gmail.com>
2017-06-17 22:21:57 -0800

    Create insert, update, delete TaiKhoan

---
+++ MobileShop/Areas/Admin/Controllers/QuanLyTaiKhoanController.cs
@@ -0,0 +1,91 @@
+﻿using MobileShop.Areas.Admin.Models.SanPhamaAdminBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MobileShop.Areas.Admin.Controllers
+{
+    public class QuanLyTaiKhoanController : Controller
+    {
+        // GET: Admin/QuanLyTaiKhoan
+        public ActionResult Index()
+        {
+            var dsTaiKhoan = QuanLyTaiKhoanBus.DanhSach();
+            return View(dsTaiKhoan);
+        }
+
+        // GET: Admin/QuanLyTaiKhoan/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/QuanLyTaiKhoan/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/QuanLyTaiKhoan/Create
+        [HttpPost]
+        public ActionResult Create(MobileShopConnection.AspNetUser us)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                QuanLyTaiKhoanBus.ThemTaiKhoan(us);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/QuanLyTaiKhoan/Edit/5
+        public ActionResult Edit(string id)
+        {
+            return View(QuanLyTaiKhoanBus.ChiTietUser(id));
+        }
+
+        // POST: Admin/QuanLyTaiKhoan/Edit/5
+        [HttpPost]
+        public ActionResult Edit(string id, MobileShopConnection.AspNetUser us)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                QuanLyTaiKhoanBus.Update(id, us);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/QuanLyTaiKhoan/Delete/5
+        public ActionResult Delete(string id)
+        {
+            return View(QuanLyTaiKhoanBus.ChiTietUser(id));
+        }
+
+        // POST: Admin/QuanLyTaiKhoan/Delete/5
+        [HttpPost]
+        public ActionResult Delete(string id, MobileShopConnection.AspNetUser us)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                QuanLyTaiKhoanBus.Delete(id, us);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MobileShop/Areas/Admin/Models/SanPhamaAdminBus/QuanLyTaiKhoanBus.cs
@@ -0,0 +1,39 @@
+﻿using MobileShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+
+namespace MobileShop.Areas.Admin.Models.SanPhamaAdminBus
+{
+    public class QuanLyTaiKhoanBus
+    {
+        public static IEnumerable<AspNetUser> DanhSach()
+        {
+            var dsLoaiTaiKhoan = new MobileShopConnectionDB();
+            return dsLoaiTaiKhoan.Query<AspNetUser>("select * from AspNetUsers");
+        }
+        public static MobileShopConnection.AspNetUser ChiTietUser(string id)
+        {
+            var db = new MobileShopConnectionDB();
+            return db.SingleOrDefault<MobileShopConnection.AspNetUser>("select * from AspNetUsers where Id = @0", id);
+        }
+        public static void Delete(string id, MobileShopConnection.AspNetUser us)
+        {
+            var db = new MobileShopConnectionDB();
+            db.Delete<AspNetUser>("where Id = @0", id);
+        }
+        public static void Update(string id, MobileShopConnection.AspNetUser us)
+        {
+            var password = Crypto.Hash(us.PasswordHash, "MD5");
+            var db = new MobileShopConnectionDB();
+            db.Update<AspNetUser>("SET Email = @0, PasswordHash = @1, PhoneNumber = @2, UserName = @3 where Id = @4",us.Email, password, us.PhoneNumber, us.UserName,id);
+        }
+        public static void ThemTaiKhoan(MobileShopConnection.AspNetUser us)
+        {
+            var db = new MobileShopConnectionDB();    
+            db.Insert(us);
+        }
+    }
+}
